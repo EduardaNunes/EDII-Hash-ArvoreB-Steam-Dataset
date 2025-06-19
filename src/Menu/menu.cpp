61700f7ea1d6b06cd6d05171820aba87bea38a01
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2025-06-19 14:26:01 -0300

    add simple initial menu

---
+++ src/Menu/menu.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "menu.h"
+
+using namespace std;
+
+void Menu::showMenu() {
+    int opcao;
+    do {
+        cout << "1 - Buscar jogador por ID" << endl;
+        cout << "2 - Mostrar top N jogadores" << endl;
+        cout << "0 - Sair" << endl;
+        cin >> opcao;
+
+        switch(opcao) {
+            case 1: /* chama busca na hash */ break;
+            case 2: /* busca na árvore B */ break;
+        }
+    } while(opcao != 0);
+}
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Menu/menu.h"
+
+using namespace std;
+
+int main(){
+
+    Menu menu;
+    menu.showMenu();
+
+    return 0;
+}
