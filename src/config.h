672bd48bf69889a5342925f1290de3caafcf9eea
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-07-06 22:16:09 -0300

    Remove caminhos dos CSVs para teste do config.h

--- src/config.h
+++ src/config.h
@@ -5,10 +5,3 @@
 #define CSV_PLAYERS_PATH "../steam/players.csv"
 #define CSV_PRICES_PATH "../steam/prices.csv"
 #define CSV_PURCHASED_GAMES_PATH "../steam/purchased_games.csv"
-
-#define CSV_ACHIEVEMENTS_TESTE_PATH "../PlanilhasDeTeste/achievements_teste.csv"
-#define CSV_GAMES_TESTE_PATH "../PlanilhasDeTeste/games_teste.csv"
-#define CSV_HISTORY_TESTE_PATH "../PlanilhasDeTeste/history_teste.csv"
-#define CSV_PLAYERS_TESTE_PATH "../PlanilhasDeTeste/players_teste.csv"
-#define CSV_PRICES_TESTE_PATH "../PlanilhasDeTeste/prices_teste.csv"
-#define CSV_PURCHASED_GAMES_TESTE_PATH "../PlanilhasDeTeste/purchased_games_teste.csv"
